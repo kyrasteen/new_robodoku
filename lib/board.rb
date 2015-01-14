a5bfc1a37ac1993d3ffc1c3d8e00b6d4ad4baeb9
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-14 16:36:07 -0700

    Added setup grid to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,22 @@
+require_relative 'row'
+
 class Board
+  attr_reader :input
 
   def initialize(input)
-
+    @input = input
+    # @row = Row.new
   end
+
+  def set_up_board
+    rows = input.split("\n")
+    rows.map do |row|
+      numbers = row.split('')
+      numbers.map do |n|
+        n.to_i
+      end
+    end
+  end
+
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -4,11 +4,16 @@ require 'minitest/pride'
 
 class BoardTest < Minitest::Test
 
-  def test_it_creates_a_row
+  def test_it_sets_up_board
     input = "123456789\n123456789"
     board = Board.new(input)
-    assert_instance_of(Row, board.make_rows)
+    assert_equal [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]], board.set_up_board
   end
 
+  def test_it_solves_rows
+    input = "120456789\n023456789"
+    board = Board.new(input)
+    assert_equal "123456789\n123456789", board.solve_rows
+  end
 
 end
